47b14233166b3a441e635f2d98963e184420f774
Alaaswabet <85838336+Alaaswabet@users.noreply.github.com>
2026-05-22 15:08:59 +0200

    lol

---
+++ script1-main/modules/remote.lua
@@ -0,0 +1,15 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local API = ReplicatedStorage:WaitForChild("API")
+
+local Remotes = {}
+
+Remotes.HoldBaby = API:WaitForChild("AdoptAPI/HoldBaby")
+Remotes.EjectBaby = API:WaitForChild("AdoptAPI/EjectBaby")
+Remotes.ActivateFurniture = API:WaitForChild("HousingAPI/ActivateFurniture")
+Remotes.UnsubscribeFromHouse = API:WaitForChild("HousingAPI/UnsubscribeFromHouse")
+Remotes.ReplicatePerformanceModifiers = API:WaitForChild("PetAPI/ReplicatePerformanceModifiers")
+Remotes.ReplicateActivePerformances = API:FindFirstChild("PetAPI/ReplicateActivePerformances")
+Remotes.ReplicateActiveReactions = API:FindFirstChild("PetAPI/ReplicateActiveReactions")
+Remotes.DataChanged = API:WaitForChild("DataAPI/DataChanged")
+
+return Remotes
